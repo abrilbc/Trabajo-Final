9abfec64401044bf86b3db203d588e615dc01a28
abrilbc <150027058+abrilbc@users.noreply.github.com>
2023-11-18 04:17:08 -0300

    Agrego hasta opción 5 del menú.

--- FinalOrdenado_BD.php
+++ FinalOrdenado_BD.php
@@ -1,6 +1,32 @@
 <?php
 
 /**
+ * Escrbir un texto en color AMARILLO
+ * @param string $texto)
+ */
+function escribirAmarillo($texto)
+{
+    echo "\e[1;37;43m $texto \e[0m";
+}
+
+/**
+ * Escrbir un texto en color GRIS
+ * @param string $texto
+ */
+function escribirGris($texto)
+{
+    echo "\e[1;34;47m $texto \e[0m";
+}
+/**
+ * Escrbir un texto en color VERDE
+ * @param string $texto)
+ */
+function escribirVerde($texto)
+{
+    echo "\e[1;37;42m $texto \e[0m";
+}
+
+/**
  * Escrbir un texto en color ROJO
  * @param string $texto
  */
@@ -38,20 +64,34 @@ function escribirRojo($texto)
  * 
  */
 function cargarPartidas() {
-    $coleccionPartidas[0] = ["palabraWordix"=> "QUESO" , "jugador" => "majo", "intentos"=> 0, "puntaje" => 0];
-    $coleccionPartidas[1] = ["palabraWordix"=> "CASAS" , "jugador" => "rudolf", "intentos"=> 3, "puntaje" => 14];
-    $coleccionPartidas[2] = ["palabraWordix"=> "CASTA" , "jugador" => "derecha", "intentos"=> 4, "puntaje" => 10];
-    $coleccionPartidas[3] = ["palabraWordix"=> "FUEGO" , "jugador" => "majo", "intentos"=> 0, "puntaje" => 1];
-    $coleccionPartidas[4] = ["palabraWordix"=> "FIBRA" , "jugador" => "rudolf", "intentos"=> 3, "puntaje" => 5];
-    $coleccionPartidas[5] = ["palabraWordix"=> "LABIO" , "jugador" => "pink2000", "intentos"=> 2, "puntaje" => 7];
-    $coleccionPartidas[6] = ["palabraWordix"=> "PLUMA" , "jugador" => "majo", "intentos"=> 0, "puntaje" => 2];
-    $coleccionPartidas[7] = ["palabraWordix"=> "RASTA" , "jugador" => "rudolf", "intentos"=> 1, "puntaje" => 3];
-    $coleccionPartidas[8] = ["palabraWordix"=> "SUCIO" , "jugador" => "pink2000", "intentos"=> 1, "    puntaje" => 10];
-    $coleccionPartidas[9] = ["palabraWordix"=> "NEGRO" , "jugador" => "pink2000", "intentos"=> 6, "    puntaje" => 0];
-
+    $coleccionPartidas[0] = ["palabraWordix "=> "QUESO" , "jugador" => "majo", "intentos"=> 0, "puntaje" => 0];
+    $coleccionPartidas[1] = ["palabraWordix "=> "CASAS" , "jugador" => "rudolf", "intentos"=> 3, "puntaje" => 4];
+    $coleccionPartidas[2] = ["palabraWordix "=> "TINTO" , "jugador" => "derecha", "intentos"=> 4, "puntaje" => 3];
+    $coleccionPartidas[3] = ["palabraWordix "=> "GOTAS" , "jugador" => "majo", "intentos"=> 0, "puntaje" => 0];
+    $coleccionPartidas[4] = ["palabraWordix "=> "FIBRA" , "jugador" => "rudolf", "intentos"=> 3, "puntaje" => 4];
+    $coleccionPartidas[5] = ["palabraWordix "=> "VERDE" , "jugador" => "pink2000", "intentos"=> 2, "puntaje" => 5];
+    $coleccionPartidas[6] = ["palabraWordix "=> "NAVES" , "jugador" => "majo", "intentos"=> 1, "puntaje" => 6];
+    $coleccionPartidas[7] = ["palabraWordix "=> "TINTO" , "jugador" => "rudolf", "intentos"=> 1, "puntaje" => 6];
+    $coleccionPartidas[8] = ["palabraWordix "=> "PISOS" , "jugador" => "pink2000", "intentos"=> 1, "puntaje" => 6];
+    $coleccionPartidas[9] = ["palabraWordix "=> "GATOS" , "jugador" => "pink2000", "intentos"=> 6, "puntaje" => 1];
     return $coleccionPartidas;
 }
 
+/**
+ * 
+ * 
+ */
+function verificarJugador($nombre) {
+    $i = 0;
+    $band = false;
+    $arregloPartidas = cargarPartidas();
+    for ($i = 0 ; $i < count($arregloPartidas) ; $i++) {
+        if ($arregloPartidas[$i]["jugador"] == $nombre) {
+            $band = true;
+        }
+    }
+    return $band;
+}
 /** Función de la colección de palabras
  * @return array
  */
@@ -67,7 +107,7 @@ function cargarColeccionPalabras()
     return ($coleccionPalabras);
 }
 
-/** Función que determina si el primer caracter de el nombre de un jugador es letra o np
+/** Función que determina si el primer caracter de el nombre de un jugador es letra o no
  * @param string $cadena
  * @return bool
  */
@@ -90,7 +130,7 @@ function primerLetra($cadena)
 function solicitarJugador(){
     $cont=0;
     do{
-        if($cont==0){
+        if($cont == 0){
             echo "Ingrese el nombre del Jugador: ";
             $jug=trim(fgets(STDIN));
         }else{
@@ -100,7 +140,7 @@ function solicitarJugador(){
         }
         $esCaracter=primerLetra($jug);
         $cont+=1;
-    }while(!($esCaracter)); 
+    } while(!($esCaracter)); 
 
     return strtolower($jug);
 }
@@ -173,11 +213,77 @@ function mostrarPartida(){
     $partidaPrint = cargarPartidas()[$numMostrar-1];
     return $partidaPrint;
 }
+
+/** Función que muestra la primer partida ganada
+ * @param $arregloPartidas
+ * @param $nombre
+ * @return int
+ */
+function mostrarPrimerPartida($arregloPartidas, $nombre) {
+    $i = 0;
+    $retorna = -1;
+    $band = false;
+    while ($band == false && $i < count($arregloPartidas)) {
+        if(($arregloPartidas[$i]["jugador"] == $nombre) && ($arregloPartidas[$i]["puntaje"] > 0)) {
+            $band = true;
+        } else {
+            $i++;
+        }
+    }
+    if ($band == true) {
+        $retorna = $i;
+    }
+    return $retorna;
+}
+
+/** Función que muestra las estadisticas de un jugador ingresado
+ * @param array $arregloPartidas
+ * @param string $nombre
+ * 
+ */
+function escribirResumenJugador($arregloPartidas, $nombre)
+{
+    $contadorPartidas = 0;
+    $puntajeTotal = 0;
+    $victorias = 0;
+    for($i = 0; $i < count($arregloPartidas) ; $i++) {
+        if ($arregloPartidas[$i]["jugador"] == $nombre) {
+            $contadorPartidas += 1;
+            $puntajeTotal += $arregloPartidas[$i]["puntaje"];
+            
+            if (($arregloPartidas[$i]["puntaje"]) > 0) {
+                $victorias += 1;
+            }
+        }
+
+    }
+    echo "***************************************************\n";
+    echo "Jugador: " . $nombre;
+    echo "\nPartidas: " . $contadorPartidas;
+    echo "\nPuntaje Total: " . $puntajeTotal;
+    echo "\nVictorias: " . $victorias;
+    echo "\nPorcentaje Victorias: " . ($victorias/$contadorPartidas*100) . "%";
+    echo "\nAdivinadas: \n";
+
+    $cont1 = 1;
+    for($i = 0; $i < count($arregloPartidas) ; $i++) {
+        if($arregloPartidas[$i]["jugador"] == $nombre) {
+            echo "            Intento " . $cont1 . ": " . $arregloPartidas[$i]["puntaje"] . "\n";
+            $cont1 = $cont1 + 1;
+        } 
+        
+    }echo "***************************************************\n";
+   
+
+}
+    
+
 // PROGRAMA PRINCIPAL
 
 
 do {
     $opcion = seleccionarOpcion();
+    $partidas = cargarPartidas();
     $palabras = cargarColeccionPalabras();
         if ($opcion <> -1) {
         switch ($opcion) {
@@ -187,7 +293,6 @@ do {
                 echo $palabraSelec . "\n\n";
                 break;
             case 2:
-                echo "Ingrese su nombre: ";
                 $jugador = solicitarJugador();
                 $palabraAleat = palabraAleatoria($jugador);
                 echo $palabraAleat . "\n\n";
@@ -196,10 +301,39 @@ do {
                 print_r(mostrarPartida());
                 break;
             case 4:
-                echo "Opción 4\n";
+                do {
+                    $jugador = solicitarJugador();
+                    if (verificarJugador($jugador) == true) {
+                        $indicePartida = mostrarPrimerPartida($partidas, $jugador);
+                        if($indicePartida == -1){
+                            escribirRojo("Este jugador no ganó ninguna partida.");
+                            echo "\n\n";
+                        }
+                        else {
+                            escribirVerde("PRIMERA PARTIDA GANADA POR ");
+                            escribirGris($jugador);
+                            echo (": ");
+                            echo "\n";
+                            print_r($partidas[$indicePartida]);
+                        }
+                    }
+                    else {
+                        escribirRojo("Nombre inválido: El jugador no ha sido encontrado en el sistema.");
+                        echo "\n";
+                    }
+                } while (verificarJugador($jugador) == false);
                 break;
             case 5:
-                echo "Opción 5\n";
+                do {
+                    $jugador = solicitarJugador();
+                    if (verificarJugador($jugador) == true) {
+                        escribirResumenJugador($partidas, $jugador);
+                    }
+                    else {
+                        escribirRojo("Nombre inválido: El jugador no ha sido encontrado en el sistema.");
+                        echo "\n";
+                    }
+                } while (verificarJugador($jugador) == false);
                 break;
             case 6:
                 echo "Opción 6\n";
